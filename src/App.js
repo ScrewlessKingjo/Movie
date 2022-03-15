c6a43336ec241f9642d1069e727c0dfe2ce0cd24
ScrewlessKingjo <bszt123@naver.com>
2022-03-15 20:15:27 +0900

    최종커밋

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
-
-function Potato() {
-  return <h1>귀찮아</h1>
-}
+import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 
 function App() {
-  return (<div>Hello <Potato /> </div>
+  return (
+    
+
+    <HashRouter>
+      <Navigation />
+      <Routes>
+      <Route path="/" exact={true} element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/movie-detail" element={<Detail />} />
+      </Routes>
+    </HashRouter>
 
   );
 }
+
 export default App;
